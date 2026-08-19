610b036460ecaeb6765254f3a5a54433a891d42d
BeZide93 <r.wolter93@gmx.de>
2026-08-19 13:18:43 +0200

    Mod: Use flow services for Midna prompts

--- mods/dawnlight/src/service_imports.hpp
+++ mods/dawnlight/src/service_imports.hpp
@@ -1,15 +1,19 @@
 #pragma once
 
 #include "mods/svc/config.h"
+#include "mods/svc/flow.h"
 #include "mods/svc/game.h"
 #include "mods/svc/hook.h"
 #include "mods/svc/host.h"
 #include "mods/svc/log.h"
+#include "mods/svc/message.h"
 #include "mods/svc/ui.h"
 
 extern const ConfigService* svc_config;
+extern const FlowService* svc_flow;
 extern const GameService* svc_game;
 extern const HookService* svc_hook;
 extern const HostService* svc_host;
 extern const LogService* svc_log;
+extern const MessageService* svc_message;
 extern const UiService* svc_ui;
